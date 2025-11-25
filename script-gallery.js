12109bfe1188b3e8239180accf07739576a63ad1
tehwalk <eiraloupi2@gmail.com>
2025-11-25 16:43:56 +0200

    added elements & basic functionality to modal

--- script-gallery.js
+++ script-gallery.js
@@ -14,8 +14,22 @@ function load_images(){
         var active_element = works[parseInt(item.dataset.number)];
         var image = item.children[0]; ///make sure the first chlid element of the div is the image!!!
         image.src = active_element.images[0];
+        item.style.cursor = 'pointer';
+        item.onclick = function() {
+            openModal(this.dataset.id);
+        };
     }
     
+}
+
+function openModal(id) {
+    document.getElementById('imageModal').style.display = 'block';
+    var work_title = document.getElementById('work-title');
+    //TODO: Make the related entry from works array to appear
+}
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', load_images)
